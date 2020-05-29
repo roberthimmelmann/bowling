6ca7aef4bcc1cce6f537c853072125e6145b1b0e
Robert Himmelmann <mail@robert-himmelmann.com>
2020-05-29 22:36:02 +0200

    Added test

--- test/bowling/BowlingTest.kt
+++ test/bowling/BowlingTest.kt
@@ -2,6 +2,7 @@ package bowling
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.test.assertNull
 
 internal class BowlingTest {
     @Test
@@ -30,5 +31,22 @@ internal class BowlingTest {
     fun testUnfinishedSpareOrStrike() {
         assertEquals(10, Bowling.fromRolls(listOf(6, 4)).frames.get(0).getPoints())
         assertEquals(12, Bowling.fromRolls(listOf(10, 2)).frames.get(0).getPoints())
+    }
+
+    @Test
+    fun testMultipleFrames() {
+        val b = Bowling.fromRolls(listOf(1, 3, 10, 5, 5, 3, 1)).frames
+
+        assertEquals(1, b.get(0).firstRoll)
+        assertEquals(3, b.get(0).secondRoll)
+        assertEquals(4, b.get(0).getPoints())
+
+        assertEquals(10, b.get(1).firstRoll)
+        assertNull(b.get(1).secondRoll)
+        assertEquals(20, b.get(1).getPoints())
+
+        assertEquals(5, b.get(2).firstRoll)
+        assertEquals(5, b.get(2).secondRoll)
+        assertEquals(13, b.get(2).getPoints())
     }
 }
